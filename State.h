db6b42203e6b6f9a1e0dfbcaed4570584f765227
Can Akinci <akincifca@gmail.com>
2022-04-12 22:51:30 +0300

    State/Pausable State interface added

---
+++ State.h
@@ -0,0 +1,28 @@
+
+#ifndef SNAKEGAME_STATE_H
+#define SNAKEGAME_STATE_H
+
+#include <SFML/System/Time.hpp>
+
+namespace Engine {
+
+class State {
+public:
+    virtual ~State() {}
+
+    virtual void Init() = 0;
+    virtual void ProcessInput() = 0;
+    virtual void Update(sf::Time deltaTime) = 0;
+    virtual void Draw() = 0;
+};
+
+class PausableState : public State {
+    virtual ~PausableState() {}
+
+    virtual void Pause() = 0;
+    virtual void Start() = 0;
+};
+
+}
+
+#endif //SNAKEGAME_STATE_H
--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
 
     while (window.isOpen())
     {
@@ -16,7 +15,6 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
         window.display();
     }
 
